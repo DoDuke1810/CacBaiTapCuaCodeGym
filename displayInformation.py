8abfab3b25c4a9d4e13f477dec7e6a6de23e62f7
DoDuke1810 <hung.20071810.903@gmail.com>
2022-08-15 10:38:22 +0100

    BT_displayInformation

---
+++ displayInformation.py
@@ -0,0 +1,4 @@
+print("Họ và tên: Đỗ Gia Hưng")
+print("Ngày sinh: 18/10/2007")
+print("Địa chỉ: 803/23/10/21 Huỳnh Tấn Phát, Phường Phú Thuận, Quận 7, Thành phố Hồ Chí Minh")
+print("Mục đích đến với khóa học: Để hướng đến ngành trí tuệ thông minh AI và Machine Learning, để tìm được một công việc ổn định trong tương lai và có một cuộc sống đầy đủ")
